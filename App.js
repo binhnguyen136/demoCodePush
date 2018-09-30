51f77ecf8fe4104cba4d0fa9452818d8eacbc4a3
Binh Nguyen <quangbinh136@gmail.com>
2018-09-30 20:00:33 +0700

    update codepush

--- App.js
+++ App.js
@@ -33,7 +33,7 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Code Push update
         </Text>
         <Text style={styles.instructions}>
           To get started, edit App.js
