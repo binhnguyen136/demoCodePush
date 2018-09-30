dad6c13ff9ce57f0d722c73b1b83b94c023e5dad
Binh Nguyen <quangbinh136@gmail.com>
2018-09-30 19:49:03 +0700

    add codepush

--- App.js
+++ App.js
@@ -12,6 +12,8 @@ import {
   View
 } from 'react-native';
 
+import codePush from 'react-native-code-push';
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
@@ -20,6 +22,13 @@ const instructions = Platform.select({
 });
 
 export default class App extends Component {
+  componentWillMount() {
+    codePush.sync({
+      updateDialog: true,
+      installMode: codePush.InstallMode.IMMEDIATE
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
